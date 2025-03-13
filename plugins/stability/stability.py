c24bfdb97cf6510989e302a20168b0d5fd0a290f
davexxx <davexxx@163.com>
2025-03-13 21:57:50 +0800

    修正google修图问题

--- plugins/stability/stability.py
+++ plugins/stability/stability.py
@@ -1004,7 +1004,7 @@ class stability(Plugin):
                     image
                 ],
                 config=types.GenerateContentConfig(
-                    response_modalities=['Image']
+                    response_modalities=['Text', 'Image']
                 )
             )
             
